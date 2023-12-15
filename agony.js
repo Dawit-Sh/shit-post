94f3f3e63eed8aedbcbb46c3584e554b34808794
nk <nk@localhost.localdomain>
2023-12-15 12:22:50 -0500

    Update

---
+++ agony.js
@@ -0,0 +1,27 @@
+function Darkmode() {
+window.alert("Don't be a pussy use WHITE MODE");
+}
+function EndIt(){
+  const popup = window.open('https://i.imgur.com/1aFz2h4.png', 'popup', 'width=400,height=300');
+}
+function PopupMore(){
+  const popup = window.open('https://i.imgur.com/1aFz2h4.png', 'popup', 'width=400,height=300');
+}
+ const form = document.getElementById('painForm');
+  
+  form.addEventListener('mouseover', function() {
+    alert('Pleb Trying to move around');
+  });
+  
+  form.addEventListener('input', function() {
+    alert('Pleb is typing in the form! 🤣');
+  });
+function HabibiCat() {
+  const popup = window.open('https://i.imgur.com/g1qUjpY.png', 'popup', 'width=400,height=300');
+  if (popup) {
+    popup.focus();
+  } else {
+    alert('Habibi Cat Wants to throw wisdom unto thee.');
+  }
+}
+//setInterval(HabibiCat, 3000); 
